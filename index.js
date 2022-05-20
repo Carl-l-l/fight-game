a919b7c80ac3ca0b99fe1cdf234376b434d9fdb7
Carl-l-l <carlleeladefoged@gmail.com>
2022-05-20 09:40:18 +0200

    player listeners and enemy creation

--- index.js
+++ index.js
@@ -6,22 +6,48 @@ canvas.height = 768;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
+const gravity = 0.7;
+
 class Sprite {
     constructor({ position, velocity }) {
         this.position = position;
         this.velocity = velocity;
+        this.height = 150;
+        this.width = 100;
     }
 
     draw() {
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.position.x, this.position.y, 50, 150);
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
     update() {
         this.draw();
-        this.position.y += 10;
+        this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
+        if (this.position.y + this.height >= canvas.height) {
+            this.velocity.y = 0;
+            this.position.y = canvas.height - this.height;
+        } else if(this.position.y <= 0) {
+            this.velocity.y = 0;
+            this.position.y = 1;
+        }
+        else {
+            this.velocity.y += gravity;
+        }
+
+        if (this.position.x + this.width >= canvas.width) {
+            this.velocity.x = 0;
+            this.position.x = canvas.width - this.width;
+        } else if(this.position.x <= 0) {
+            this.velocity.x = 0;
+            this.position.x = 0;
+        }
     }
 }
+
+
 
 const player = new Sprite({
     position: {
@@ -47,12 +73,44 @@ const enemy = new Sprite({
 
 function animate() {
     window.requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
 }
 
 
 animate();
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    switch (e.key) {
+        case 'ArrowUp':
+            player.velocity.y = -10;
+            break;
+        case 'ArrowDown':
+            player.velocity.y = 10;
+            break;
+        case 'ArrowLeft':
+            player.velocity.x = -10;
+            break;
+        case 'ArrowRight':
+            player.velocity.x = 10;
+            break;
+        case 'w':
+            player.velocity.y = -10;
+            break;
+        case 's':
+            player.velocity.y = 10;
+            break;
+        case 'a':
+            player.velocity.x = -10;
+            break;
+        case 'd':
+            player.velocity.x = 10;
+            break;
+
+    }
+})
 
 console.log(player);
